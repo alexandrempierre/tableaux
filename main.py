5ed22b2c807b9be7324e74441122b923e44a875b
Alexandre Pierre <alexandrempierre@gmail.com>
2017-07-19 21:27:19 -0300

    Resolvidos: entrada com código prolixo e interação com usuário bagunçada

--- main.py
+++ main.py
@@ -5,47 +5,83 @@ from nltk import *
 
 read_expr = Expression.fromstring
 
-def manual_input_indiv_tab():
+def input_func(file=False,read=read_expr,input_msg=None):
+	if input_msg is None:
+		input_msg = ''
+
+	read_file_line = lambda: read(file.readline().strip())
+	read_input = lambda: read(input(input_msg).strip())
+
+	while True:
+		try:
+			if file:
+				return read_file_line()
+
+			return read_input()
+		except ValueError:
+			print('Press Ctrl+C to close the program')
+			continue
+		except KeyboardInterrupt:
+			print('\n\nProgram closed by the user...\n')
+			exit()
+
+def get_qt_problems(file=False):
+	return input_func(file=file, read=int, \
+		input_msg='Number of tableauxs to solve: ')
+
+def get_goal(file=False):
+	return input_func(file=file, read=read_expr, \
+		input_msg='Affirmation to deduce: ')
+
+def get_assumptions(file=False):
+	number = input_func(file=file, read=int, \
+		input_msg='Number of assumptions given: ')
+
 	assumptions = []
+	for i in range(1,number+1):
+		assumptions.append(input_func(file=file, read=read_expr, \
+			input_msg='Assumption {0}/{1}: '.format(i, number)))
 
-	goal = read_expr(input('Affirmation to deduce: '))
-	num_assumptions = int(input('Number of assumptions given: '))
+	return assumptions
 
-	for i in range(1,num_assumptions+1):
-		assumptions.append(read_expr(input('Assumption {0}/{1}: '.format(i,num_assumptions))))
+def get_problem(file=False):
+	goal = get_goal(file)
+	assumptions = get_assumptions(file)
+
+	if file:
+		print('Goal: {0}'.format(goal))
+		print('Assumptions:\n')
+		for assumption in assumptions:
+			print(assumption)
+
 	return goal,assumptions
 
-def manual_input_all():
-	problems = int(input('Number of tableauxs to solve: '))
-	for i in range(problems):
-		goal,assumptions = manual_input_indiv_tab()
+def problems(file=False):
+	qt_problems = get_qt_problems(file)
 
-		print(tableaux(goal=goal, assumptions=assumptions))
+	for i in range(1,qt_problems+1):
+		print("\n===================== Problem {0}/{1} =====================\n".format(i, qt_problems))
+		goal, assumptions = get_problem(file)
+		if tableaux(goal, assumptions):
+			print("\nThis tableaux is closed")
+		else:
+			print("\nThis tableaux can't be closed")
+
+	print("\nNo more problems to solve\nClosing...\n")
 
 if __name__ == '__main__':
 	from sys import argv
 
 	if len(argv) == 1:
-		manual_input_all()
+		problems()
 	else:
 		file_name = argv[1]
-
 		try:
 			with open(file_name) as f:
-				problems = int(f.readline().strip())
-				for i in range(problems):
-					goal = f.readline().strip()
-					while goal == '':
-						goal = f.readline().strip()
-					goal = read_expr(goal)
-					num_assumptions = int(f.readline().strip())
-					assumptions = []
-					for i in range(1,num_assumptions+1):
-						assumptions.append(read_expr(f.readline().strip()))
-					print(tableaux(goal=goal, assumptions=assumptions))
+				problems(f)
 		except FileNotFoundError:
 			ans = input('File not found, want to manually enter the necessary data? (yes/[no]) ')
-			if (ans or 'no').lower() not in ['no','dont',"don't",'not','0','false']:
-				manual_input_all()
+			if ans.strip() in ['yes', 'sim', 'y', 's', 'prox', 'proximo', 'next', 'p', '1']:
+				problems()
 			else:
 				print('See ya!')
